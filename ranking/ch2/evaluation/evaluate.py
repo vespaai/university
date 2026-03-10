2b05dda54f336bbd2039c50883a71b0b0b5273a0
Radu Gheorghe <radu@vespa.ai>
2026-03-10 18:39:33 +0200

    reverted evaluate to lexical by default to fit the class flow

--- ranking/ch2/evaluation/evaluate.py
+++ ranking/ch2/evaluation/evaluate.py
@@ -82,8 +82,8 @@ def hybrid_search(query_text: str, top_k: int) -> dict:
         "ranking.profile": "hybrid"
     }
 
-# QUERY_FUNCTION = lexical_search
-QUERY_FUNCTION = vector_search
+QUERY_FUNCTION = lexical_search
+# QUERY_FUNCTION = vector_search
 # QUERY_FUNCTION = hybrid_search
 
 ########################################################
